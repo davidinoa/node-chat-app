678e86d2402284997c3e3d186a6e8b38c0a6df4c
David Inoa <davidinoa@gmail.com>
2018-06-02 13:15:03 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Dave',
-		text: 'Hey! This is Dave'
-	});
 });
 
 socket.on('disconnect', () => {
