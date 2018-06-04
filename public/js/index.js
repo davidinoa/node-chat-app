83df1a8900db5bad3997ea7afad37489877ebb1d
David Inoa <davidinoa@gmail.com>
2018-06-04 11:05:54 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,14 +29,16 @@ socket.on('newLocationMessage', (message) => {
 
 $('#message-form').on('submit', (e) => {
 	e.preventDefault();
-
+	const messageTextbox = $('[name=message]');
 	socket.emit(
 		'createMessage',
 		{
 			from: 'User',
-			text: $('[name=message]').val()
+			text: messageTextbox.val()
 		},
-		() => {}
+		() => {
+			messageTextbox.val('');
+		}
 	);
 });
 
@@ -45,14 +47,19 @@ locationButton.on('click', () => {
 	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
+
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(
 		(position) => {
+			locationButton.removeAttr('disabled').text('Send location');
 			socket.emit('createLocationMessage', {
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude
 			});
 		},
 		() => {
+			locationButton.removeAttr('disabled').text('Send location');
 			alert('Unable to fetch location.');
 		}
 	);
